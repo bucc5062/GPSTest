d152538f9a4aef7b2e9ee5e7c0a845dac427919c
justin hull <bucc5062@gmail.com>
2015-12-11 15:36:27 -0500

    Initial commit

---
+++ app/src/main/java/com/newproject/jhull3341/gpstest/MainActivity.java
@@ -0,0 +1,42 @@
+package com.newproject.jhull3341.gpstest;
+
+import android.location.Location;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+
+public class MainActivity extends AppCompatActivity
+                          implements LocationListener,
+                                    GoogleApiClient.ConnectionCallbacks,
+                                    GoogleApiClient.OnConnectionFailedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+}
